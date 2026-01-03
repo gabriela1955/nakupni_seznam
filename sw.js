3166bbda303156a169e266ec19f8306a9f941b0b
gabriela1955 <gabriela.hasova@gmail.com>
2026-01-03 18:38:08 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,12 @@
-const CACHE = "nakupni-seznam-v21";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+const CACHE = "nakupni-seznam-v22";
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./sw.js","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
-    await self.clients.claim();
-  })());
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (e) => {
